67a71d56c81387015d78d31eb4ee97e6b12edf04
zitraxblade <egor50311@gmail.com>
2025-12-03 19:39:57 +0300

    fix

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,17 +1,28 @@
 import fs from 'fs';
 import path from 'path';
+import yaml from 'js-yaml';
 import { buildDiff } from './buildDiff.js';
 import { formatters } from './formatters/index.js';
+
+const parseFile = (filepath) => {
+  const ext = path.extname(filepath).toLowerCase();
+  const content = fs.readFileSync(filepath, 'utf-8');
+
+  if (ext === '.json') return JSON.parse(content);
+  if (ext === '.yml' || ext === '.yaml') return yaml.load(content);
+
+  throw new Error(`Unsupported file type: ${ext}`);
+};
 
 export const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const fullPath1 = path.resolve(process.cwd(), filepath1);
   const fullPath2 = path.resolve(process.cwd(), filepath2);
 
-  const data1 = JSON.parse(fs.readFileSync(fullPath1, 'utf-8'));
-  const data2 = JSON.parse(fs.readFileSync(fullPath2, 'utf-8'));
+  const data1 = parseFile(fullPath1);
+  const data2 = parseFile(fullPath2);
 
   const diffTree = buildDiff(data1, data2);
 
-  const formatter = formatters[formatName] || formatters.stylish; // выбираем формат
+  const formatter = formatters[formatName] || formatters.stylish;
   return formatter(diffTree);
 };
