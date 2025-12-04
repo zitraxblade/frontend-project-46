d675c8ba627bb7ee6246614d7de80f0502374ab2
zitraxblade <egor50311@gmail.com>
2025-12-04 16:17:35 +0300

    сохроняю перед сносом

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,14 +3,16 @@ import { Command } from 'commander';
 import gendiff from '../src/gendiff.js';
 
 const program = new Command();
+
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format', 'stylish')
-  .action((filepath1, filepath2) => {
-    console.log(gendiff(filepath1, filepath2, program.format));
+  .action((filepath1, filepath2, options) => {
+ 
+    console.log(gendiff(filepath1, filepath2, options.format));
   });
 
 program.parse(process.argv);
